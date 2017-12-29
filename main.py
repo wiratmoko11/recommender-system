47be3b25ad4ec231cc9d08815206efa8b86e253a
sadewa25 <wicaksana_irfan@yahoo.com>
2017-12-29 21:24:58 +0700

    Rating dan Kolom Sukses

--- main.py
+++ main.py
@@ -16,11 +16,19 @@ class mainApp(QtWidgets.QMainWindow, view.Ui_bgDialog):
 		# Data Untuk Generate Kolom
 		self.dataID = []
 		self.dataMovieID = ""
+		# Data Untuk Generate Baris
+		self.dataUserID=""
 
 	def generateKolom(self):
 		self.table_data.setColumnCount(len(self.dataID))
 		for i in range(len(self.dataID)):
 			self.table_data.setHorizontalHeaderItem(i, QtWidgets.QTableWidgetItem(str(self.dataID[i])))
+
+	def generateRating(self,dataUserID):
+		self.table_data.setRowCount(len(dataUserID))
+		for i in range(0,len(dataUserID)):
+			for j in range(0,i):
+				self.table_data.setItem(i,j,QtWidgets.QTableWidgetItem(j))
 
 	def openFile(self):
 		# Open File
@@ -31,8 +39,16 @@ class mainApp(QtWidgets.QMainWindow, view.Ui_bgDialog):
 		rmDuplicate = set(self.dataMovieID)
 		self.dataID = list(rmDuplicate)
 		self.dataID.sort()
+		# getData UserID
+		self.dataUserID = self.data.loc[:,('userId')].values
+		self.dataUserID.sort()
+		barisDup = set(self.dataUserID)
+		userListID = list(barisDup)
+		userListID.sort()
+		print(userListID)
 		# Func Generate Kolom
 		self.generateKolom()
+		self.generateRating(userListID)
 		
 def main():
     app = QtWidgets.QApplication(sys.argv)  # A new instance of QApplication
